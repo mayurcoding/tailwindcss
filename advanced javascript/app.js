9c637b5c068fc5a02ab6c8a98a9551bd14827374
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-03-18 20:45:27 +0530

    ex1

--- advanced javascript/app.js
+++ advanced javascript/app.js
@@ -1,10 +1,8 @@
 // console.log(value);
 // var value = "My data";
 
-
 // add(10,20);
 // // calling
-
 
 // // declaration
 // function add(a,b){
@@ -13,11 +11,10 @@
 
 // function greet(morning){
 //     return morning();
-// }   
+// }
 // greet(()=>{
 //     console.log("Good Morning")
 // });
-
 
 // let greet = ()=>
 // {
@@ -26,8 +23,34 @@
 
 // setTimeout(greet,4000);
 
-
 // setTimeout(()=>{
 //     console.log("Good Morning");
 // },4000);
 // console.log("I am second good morning");
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let addNumber5 = numbers.map((number)=>
+// {       return number+10;
+
+// });
+// console.log(numbers)
+// console.log(addNumber5);
+
+
+// filter
+// let greaterThan5 = numbers.filter((number) => {
+//   return number > 5;
+// });
+// console.log(numbers);
+// console.log(greaterThan5);
+
+let marks = [67, 54, 24 ,66, 43, 38, 32, 39];
+
+let graceMarks = marks.filter((mark) => {
+    return mark < 40;
+}).map((mark) => {
+    return mark + 10; 
+}
+);  
+console.log(graceMarks);
---
+++ advanced javascript/js exercise/app1.js
@@ -0,0 +1,7 @@
+console.log(" js snippet");
+
+
+const numbers = [1, 2, 3, 4, 5,];
+
+const result = numbers.map(num=>num*2);
+console.log(result);
